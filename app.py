420a62c972910137ab02d370ec8cdcb984fa5285
Rohit Nimkar <nehalnimkar@gmail.com>
2020-07-07 10:24:23 +0530

    handled coflicting codes using flash messages

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
+import os.path
 
 app = Flask(__name__)
+app.secret_key = 'lslsknljnjdjjdljgndl'
 
 @app.route('/')
 def home():
@@ -10,6 +12,14 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     urls = {}
+    if os.path.exists('urls.json'):
+        with open('urls.json') as url_file:
+            urls = json.load(url_file)
+    if request.form['code'] in urls:
+        flash('That shortname has already taken. Please select another one.')
+        return redirect(url_for('home'))
+
+
     urls[request.form['code']] = {'url': request.form['url']}
     with open('urls.json', 'w') as url_file:
         json.dump(urls, url_file)
