81ef12cc4325e7b202ae046ec339da9d5e9d0b56
Rohit Nimkar <nehalnimkar@gmail.com>
2020-07-07 10:10:31 +0530

    save data code and url to json file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -8,7 +9,12 @@ def home():
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
+    urls = {}
+    urls[request.form['code']] = {'url': request.form['url']}
+    with open('urls.json', 'w') as url_file:
+        json.dump(urls, url_file)
+
     if request.method == 'POST':
         return render_template('your_url.html', code=request.form['code'])
     else: 
-        return 'Invalid request'
+        return redirect(url_for('home'))
